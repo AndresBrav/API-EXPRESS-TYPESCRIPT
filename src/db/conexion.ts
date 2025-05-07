072a0c61592f71118e78c2790896a20b7f6d42e6
Andres Bravo <bravoandres1706@gmail.com>
2025-05-07 09:32:47 -0400

    cambio del host

--- src/db/conexion.ts
+++ src/db/conexion.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 // Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize("carroscrudts", "root", "", {
-    host: "localhost",
+    host: "127.0.0.1",
     dialect: "mysql",
 });
 
