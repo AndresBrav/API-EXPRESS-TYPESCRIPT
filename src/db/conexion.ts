f7d59c549e20e94250e1b49d155808efbe736913
Andres Bravo <bravoandres1706@gmail.com>
2025-05-07 09:35:52 -0400

    hostdb añadido a .env

--- src/db/conexion.ts
+++ src/db/conexion.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
+import * as dotenv from 'dotenv';
+dotenv.config(); // ¡Esto carga el archivo .env!
 
 // Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize("carroscrudts", "root", "", {
-    host: "127.0.0.1",
+    host: process.env.HOSTDB || 'localhost',
     dialect: "mysql",
 });
 
