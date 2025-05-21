541447994202a75b6f08e6533937f8cf323aefb8
Andres Bravo <bravoandres1706@gmail.com>
2025-05-21 09:12:44 -0400

    schedules.ts updated

--- src/Tasks/scheduler.ts
+++ src/Tasks/scheduler.ts
@@ -1,8 +1,8 @@
 import cron from 'node-cron'
 import { downloadAutomaticFiles } from '../Services/cars/schedulerCars';
 
-cron.schedule('* * * * *', async () => {
-    // console.log('this task is executed every minute');
+cron.schedule('*/2 * * * *', async () => {
+    // console.log('this task is executed every 2 minute');
 
     downloadAutomaticFiles()    /* download from ftp */
 });
