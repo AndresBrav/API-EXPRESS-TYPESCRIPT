5287718d4accdf50b5a2c5a6defdb45bd54d90d9
Andres Bravo <bravoandres1706@gmail.com>
2025-06-18 11:08:32 -0400

    first version connection with postgresql

--- src/db/conexion.ts
+++ src/db/conexion.ts
@@ -3,9 +3,13 @@ import * as dotenv from 'dotenv';
 dotenv.config(); // ¡Esto carga el archivo .env!
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("carroscrudts", "root", "", {
+// const sequelize = new Sequelize("carroscrudts", "root", "", {
+//     host: process.env.HOSTDB || 'localhost',
+//     dialect: "mysql",
+// });
+const sequelize = new Sequelize("carroscrudts", "postgres", "", {
     host: process.env.HOSTDB || 'localhost',
-    dialect: "mysql",
+    dialect: "postgres",
 });
 
 export default sequelize
