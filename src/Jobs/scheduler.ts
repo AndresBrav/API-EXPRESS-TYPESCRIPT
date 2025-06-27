6fd9387bd82cd9b2c4abfe35f152726fcc0f24a7
Andres Bravo <bravoandres1706@gmail.com>
2025-06-27 10:14:50 -0400

    work with next js

--- src/Jobs/scheduler.ts
+++ src/Jobs/scheduler.ts
@@ -8,31 +8,15 @@ cron.schedule('0 */2 * * *', async () => {
     /* download from ftp */
     await downloadAutomaticFiles();
 });
-
-// cron.schedule('*/10 * * * * *', async () => {
-//     /* download from ftp */
-//     await downloadAutomaticFiles();
-// });
-
 // every 2 hours 
 cron.schedule('0 */2 * * *', async () => {
     await interpretedAuthomaticFiles();
 });
 
-// // every 3 hours
-// cron.schedule('0 */3 * * *', async () => {
-//     await interpretedAuthomaticFiles();
-// });
-
 // every 2 hours 
 cron.schedule('0 */2 * * *', async () => {
     await processedAuthomticFiles();
 });
-
-// cron.schedule('*/10 * * * * *',async () => {
-//     await reloadDataBase()
-// })
-
 // every 2 hours
 cron.schedule('0 */2 * * *', async () => {
     await reloadDataBase();
